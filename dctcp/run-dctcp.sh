46d3bed7547b29e7651e11f4e8d1d188bf029c61
Nikhil Handigol <nikhilh@stanford.edu>
2012-05-31 16:11:28 -0700

    update run dctcp script

--- dctcp/run-dctcp.sh
+++ dctcp/run-dctcp.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 bw=100
-t=60
+t=30
+n=3
 
 function tcp {
 # 2 TCP sources
-odir=tcp-n3-bw$bw
-#sudo python dctcp.py --bw $bw --maxq 1000 --dir $odir -t $t -n 3
+odir=tcp-n$n-bw$bw
+sudo python dctcp.py --bw $bw --maxq 1000 --dir $odir -t $t -n $n
 sudo python ../util/plot_rate.py --maxy $bw -f $odir/txrate.txt -o $odir/rate.png
 sudo python ../util/plot_queue.py -f $odir/qlen_s1-eth1.txt -o $odir/qlen.png
 sudo python ../util/plot_tcpprobe.py -f $odir/tcp_probe.txt -o $odir/cwnd.png
@@ -14,8 +15,8 @@ sudo python ../util/plot_tcpprobe.py -f $odir/tcp_probe.txt -o $odir/cwnd.png
 
 function ecn {
 # 2 TCP-ECN sources
-odir=tcpecn-n3-bw$bw
-sudo python dctcp.py --bw $bw --maxq 1000 --dir $odir -t $t -n 3 --ecn
+odir=tcpecn-n$n-bw$bw
+sudo python dctcp.py --bw $bw --maxq 1000 --dir $odir -t $t -n $n --ecn
 sudo python ../util/plot_rate.py --maxy $bw -f $odir/txrate.txt -o $odir/rate.png
 sudo python ../util/plot_queue.py -f $odir/qlen_s1-eth1.txt -o $odir/qlen.png
 sudo python ../util/plot_tcpprobe.py -f $odir/tcp_probe.txt -o $odir/cwnd.png
@@ -23,8 +24,8 @@ sudo python ../util/plot_tcpprobe.py -f $odir/tcp_probe.txt -o $odir/cwnd.png
 
 function dctcp {
 # 2 DCTCP sources
-odir=dctcp-n3-bw$bw
-sudo python dctcp.py --bw $bw --maxq 1000 --dir $odir -t $t -n 3 --dctcp
+odir=dctcp-n$n-bw$bw
+sudo python dctcp.py --bw $bw --maxq 1000 --dir $odir -t $t -n $n --dctcp
 sudo python ../util/plot_rate.py --maxy $bw -f $odir/txrate.txt -o $odir/rate.png
 sudo python ../util/plot_queue.py -f $odir/qlen_s1-eth1.txt -o $odir/qlen.png
 sudo python ../util/plot_tcpprobe.py -f $odir/tcp_probe.txt -o $odir/cwnd.png
