f0334b60656963bc09157da642c7a20196c04aca
Nikhil Handigol <nikhilh@stanford.edu>
2012-05-31 01:19:08 -0700

    update bash script to run dctcp

--- dctcp/run-dctcp.sh
+++ dctcp/run-dctcp.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 
+bw=100
+t=60
+
 function tcp {
 # 2 TCP sources
-sudo python dctcp.py --bw 1000 --speedup-bw 1013.4 --maxq 1000 --dir tcp-n3-bw1000 -t 30 -n 3
-sudo python ../util/plot_rate.py -f tcp-n3-bw1000/txrate.txt --rx -o tcp-n3-bw1000/rate.png
-sudo python ../util/plot_queue.py -f tcp-n3-bw1000/qlen_s1-eth1.txt -o tcp-n3-bw1000/qlen.png
+odir=tcp-n3-bw$bw
+#sudo python dctcp.py --bw $bw --maxq 1000 --dir $odir -t $t -n 3
+sudo python ../util/plot_rate.py --maxy $bw -f $odir/txrate.txt -o $odir/rate.png
+sudo python ../util/plot_queue.py -f $odir/qlen_s1-eth1.txt -o $odir/qlen.png
+sudo python ../util/plot_tcpprobe.py -f $odir/tcp_probe.txt -o $odir/cwnd.png
 }
 
 function ecn {
 # 2 TCP-ECN sources
-sudo python dctcp.py --bw 1000 --speedup-bw 1013.4 --maxq 1000 --dir tcpecn-n3-bw1000 -t 30 -n 3 --ecn
-sudo python ../util/plot_rate.py -f tcpecn-n3-bw1000/txrate.txt --rx -o tcpecn-n3-bw1000/rate.png
-sudo python ../util/plot_queue.py -f tcpecn-n3-bw1000/qlen_s1-eth1.txt -o tcpecn-n3-bw1000/qlen.png
+odir=tcpecn-n3-bw$bw
+sudo python dctcp.py --bw $bw --maxq 1000 --dir $odir -t $t -n 3 --ecn
+sudo python ../util/plot_rate.py --maxy $bw -f $odir/txrate.txt -o $odir/rate.png
+sudo python ../util/plot_queue.py -f $odir/qlen_s1-eth1.txt -o $odir/qlen.png
+sudo python ../util/plot_tcpprobe.py -f $odir/tcp_probe.txt -o $odir/cwnd.png
 }
 
 function dctcp {
 # 2 DCTCP sources
-sudo python dctcp.py --bw 1000 --speedup-bw 1013.4 --maxq 1000 --dir dctcp-n3-bw1000 -t 30 -n 3 --dctcp
-sudo python ../util/plot_rate.py -f dctcp-n3-bw1000/txrate.txt --rx -o dctcp-n3-bw1000/rate.png
-sudo python ../util/plot_queue.py -f dctcp-n3-bw1000/qlen_s1-eth1.txt -o dctcp-n3-bw1000/qlen.png
+odir=dctcp-n3-bw$bw
+sudo python dctcp.py --bw $bw --maxq 1000 --dir $odir -t $t -n 3 --dctcp
+sudo python ../util/plot_rate.py --maxy $bw -f $odir/txrate.txt -o $odir/rate.png
+sudo python ../util/plot_queue.py -f $odir/qlen_s1-eth1.txt -o $odir/qlen.png
+sudo python ../util/plot_tcpprobe.py -f $odir/tcp_probe.txt -o $odir/cwnd.png
 }
 
 tcp
-ecn
-dctcp
+#ecn
+#dctcp
