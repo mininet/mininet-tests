95cae2b9175439fe97f715cedd3b9abac217a509
Vimalkumar <j.vimal@gmail.com>
2012-06-04 21:29:41 -0700

    Link dequeues now do CCDF (complementary CDF); and logscale yaxis

--- dctcp/verify/link_dequeues.py
+++ dctcp/verify/link_dequeues.py
@@ -32,7 +32,20 @@ parser.add_argument('--out', '-o',
 parser.add_argument('--title',
                     default="CDF of percentage of inter-dequeue time\ndeviation from that of an ideal link")
 
+parser.add_argument('--ccdf',
+                    action="store_true",
+                    help="Plot the complementary CDF (CCDF) function.",
+                    default=False)
+
+parser.add_argument('--log',
+                    action="store_true",
+                    help="logscale y-axis.",
+                    default=False)
+
 args = parser.parse_args()
+
+if args.ccdf:
+    args.title = args.title.replace("CDF", "CCDF")
 
 def read_samples(f):
     lines = [float(line) for line in open(f).xreadlines()]
@@ -44,14 +57,24 @@ for f,exp,label in zip(args.files, args.expected, args.labels):
     normalised = [(sample / exp - 1.0) * 100.0 for sample in samples]
 
     x, y = cdf(normalised)
+    if args.ccdf:
+        y = map(lambda e: 1.0 - e, y)
     plt.plot(x, y, lw=2, label=label)
 
-plt.legend(loc="lower right")
+if args.ccdf:
+    plt.legend()
+else:
+    plt.legend(loc="lower right")
+
 plt.xlabel("Percentage deviation from expected")
 plt.ylabel("Fraction")
 plt.title(args.title)
 plt.xscale("log")
 plt.ylim((0, 1))
+
+if args.log:
+    plt.yscale('log')
+    plt.ylim((1e-3, 1))
 plt.grid(True)
 
 if args.out:
