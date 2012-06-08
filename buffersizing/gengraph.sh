93ae18c23572e4442e6e99dda878ce1d5bfbea4f
Vimalkumar <j.vimal@gmail.com>
2012-06-08 10:16:47 -0700

    Sample script to generate plot for paper

---
+++ buffersizing/gengraph.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Exit on any failure...
+set -e
+
+dir=buffersizing-Jun08-00:37 
+
+nflows="10 100 400"
+r=1
+
+function gendata {
+    for flow in $nflows; do
+        echo $flow
+
+        if ! [ -f $dir/nf$flow-r$r/mntrace_trimmed ]; then
+          echo "unzipping..."
+          gunzip $dir/nf$flow-r$r/mntrace_trimmed.gz
+        fi
+
+        python ../tracing/parse.py -f $dir/nf$flow-r$r/mntrace_trimmed --start 28 --end 30 \
+          --intf s0-eth1 --output_link_data linkdata-$flow --logscale \
+          --plots links,linkwindow --odir $dir/nf$flow-r$r/plots
+
+        gzip $dir/nf$flow-r$r/mntrace_trimmed &
+    done
+
+    wait
+}
+
+#gendata
+
+python link_dequeues.py --files linkdata-10 linkdata-100 linkdata-400 \
+    --expected  192 192 192 \
+    --labels "20 flows" "200 flows" "800 flows" \
+    --title "" --ccdf --log --out output.pdf
