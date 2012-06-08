850f633420416f023840c41a39c5eb5931480c08
Vimalkumar <j.vimal@gmail.com>
2012-06-08 12:22:26 -0700

    link dequeues to plot percent whenever possible

--- dctcp/verify/link_dequeues.py
+++ dctcp/verify/link_dequeues.py
@@ -1,5 +1,6 @@
 import sys
 
+sys.path.append('../util')
 sys.path.append('../../util')
 from helper import *
 import plot_defaults
@@ -37,12 +38,21 @@ parser.add_argument('--ccdf',
                     help="Plot the complementary CDF (CCDF) function.",
                     default=False)
 
+parser.add_argument('--percent',
+                    action="store_true",
+                    help="y-axis = percent",
+                    default=False)
+
 parser.add_argument('--log',
                     action="store_true",
                     help="logscale y-axis.",
                     default=False)
 
 args = parser.parse_args()
+PHI=1.618
+fig = plt.figure(figsize=(8, 8/PHI))
+
+LINESTYLES=['-','--','-.']
 
 if args.ccdf:
     args.title = args.title.replace("CDF", "CCDF")
@@ -51,7 +61,9 @@ def read_samples(f):
     lines = [float(line) for line in open(f).xreadlines()]
     return lines
 
+i = -1
 for f,exp,label in zip(args.files, args.expected, args.labels):
+    i += 1
     samples = read_samples(f)
     exp = float(exp)
     normalised = [abs(sample / exp - 1.0) * 100.0 for sample in samples]
@@ -59,14 +71,14 @@ for f,exp,label in zip(args.files, args.expected, args.labels):
     x, y = cdf(normalised)
     if args.ccdf:
         y = map(lambda e: 1.0 - e, y)
-    plt.plot(x, y, lw=2, label=label)
+    plt.plot(x, y, lw=2, label=label, ls=LINESTYLES[i])
 
 if args.ccdf:
     plt.legend()
 else:
     plt.legend(loc="lower right")
 
-plt.xlabel("Percentage deviation from expected")
+plt.xlabel("Percentage deviation from expected", fontsize='large')
 plt.ylabel("Fraction")
 plt.title(args.title)
 plt.xscale("log")
@@ -76,6 +88,25 @@ if args.log:
     plt.yscale('log')
     plt.ylim((1e-3, 1))
 plt.grid(True)
+
+if args.percent:
+    plt.ylabel("Percent")
+    locs, labels = plt.yticks()
+    labels = []
+    for loc in locs:
+        labels.append('%d' % (loc * 100))
+    plt.yticks(locs, labels)
+
+
+    locs, labels = plt.xticks()
+    replace = True
+    labels = []
+    for loc in locs:
+        labels.append('%d' % (loc * 100))
+        if loc * 100 > 1000:
+            replace = False
+    if replace:
+        plt.xticks(locs, labels)
 
 if args.out:
     plt.savefig(args.out)
