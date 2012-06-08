936a0de6c5c3135f189f98403e8773883382867f
Nikhil Handigol <nikhilh@stanford.edu>
2012-06-07 19:55:29 -0700

    remove unnecessary scripts

--- dctcp/hw_data/gengraph.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for opts in "--cdf" ""; do
-  plot_queue.py -f qlen-mn-dctcp.txt q-dctcp-plot.txt -l Mininet-Hifi Hardware $opts -o dctcp$opts.pdf
-  plot_queue.py -f qlen-mn-tcp.txt q-tcp-plot.txt -l Mininet-Hifi Hardware $opts -o tcp$opts.pdf
-done
-
