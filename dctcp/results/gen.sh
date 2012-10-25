2557fb16b9900a65b441afb5cc05f3563fb18020
Brandon Heller <brandonh@stanford.edu>
2012-10-24 23:05:52 -0700

    HiFi, not Hifi

--- dctcp/results/gen.sh
+++ dctcp/results/gen.sh
@@ -3,7 +3,7 @@
 pq="python ../../util/plot_queue.py"
 
 for opts in "--cdf" ""; do
-	$pq  -f qlen-mn-dctcp.txt q-dctcp-plot.txt -l Mininet-Hifi Hardware $opts -o dctcp$opts.pdf --maxy 40
-	$pq  -f qlen-mn-tcp.txt q-tcp-plot.txt -l Mininet-Hifi Hardware $opts -o tcp$opts.pdf --maxy 500
+	$pq  -f qlen-mn-dctcp.txt q-dctcp-plot.txt -l Mininet-HiFi Hardware $opts -o dctcp$opts.pdf --maxy 40
+	$pq  -f qlen-mn-tcp.txt q-tcp-plot.txt -l Mininet-HiFi Hardware $opts -o tcp$opts.pdf --maxy 600
 done
 
