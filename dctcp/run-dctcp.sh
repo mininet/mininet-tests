18dcd8c2ddb54dc79d7c8d11879118d58f9a7763
Brandon Heller <brandonh@stanford.edu>
2012-10-30 17:21:24 -0700

    Generalize script to run DCTCP tests

--- dctcp/run-dctcp.sh
+++ dctcp/run-dctcp.sh
@@ -1,37 +1,39 @@
 #!/bin/bash
 
-bw=100
-t=120
+bws="100 1000"
+t=20
 n=3
 maxq=425
 
 function tcp {
-# 2 TCP sources
-odir=tcp-n$n-bw$bw
-sudo python dctcp.py --bw $bw --maxq $maxq --dir $odir -t $t -n $n
-sudo python ../util/plot_rate.py --maxy $bw -f $odir/txrate.txt -o $odir/rate.png
-sudo python ../util/plot_queue.py -f $odir/qlen_s1-eth1.txt -o $odir/qlen.png
-sudo python ../util/plot_tcpprobe.py -f $odir/tcp_probe.txt -o $odir/cwnd.png
+	bw=$1
+	odir=tcp-n$n-bw$bw
+	sudo python dctcp.py --bw $bw --maxq $maxq --dir $odir -t $t -n $n
+	sudo python ../util/plot_rate.py --maxy $bw -f $odir/txrate.txt -o $odir/rate.png
+	sudo python ../util/plot_queue.py -f $odir/qlen_s1-eth1.txt -o $odir/qlen.png
+	sudo python ../util/plot_tcpprobe.py -f $odir/tcp_probe.txt -o $odir/cwnd.png
 }
 
 function ecn {
-# 2 TCP-ECN sources
-odir=tcpecn-n$n-bw$bw
-sudo python dctcp.py --bw $bw --maxq $maxq --dir $odir -t $t -n $n --ecn
-sudo python ../util/plot_rate.py --maxy $bw -f $odir/txrate.txt -o $odir/rate.png
-sudo python ../util/plot_queue.py -f $odir/qlen_s1-eth1.txt -o $odir/qlen.png
-sudo python ../util/plot_tcpprobe.py -f $odir/tcp_probe.txt -o $odir/cwnd.png
+	bw=$1
+	odir=tcpecn-n$n-bw$bw
+	sudo python dctcp.py --bw $bw --maxq $maxq --dir $odir -t $t -n $n --ecn
+	sudo python ../util/plot_rate.py --maxy $bw -f $odir/txrate.txt -o $odir/rate.png
+	sudo python ../util/plot_queue.py -f $odir/qlen_s1-eth1.txt -o $odir/qlen.png
+	sudo python ../util/plot_tcpprobe.py -f $odir/tcp_probe.txt -o $odir/cwnd.png
 }
 
 function dctcp {
-# 2 DCTCP sources
-odir=dctcp-n$n-bw$bw
-sudo python dctcp.py --bw $bw --maxq $maxq --dir $odir -t $t -n $n --dctcp
-sudo python ../util/plot_rate.py --maxy $bw -f $odir/txrate.txt -o $odir/rate.png
-sudo python ../util/plot_queue.py --maxy 50 -f $odir/qlen_s1-eth1.txt -o $odir/qlen.png
-sudo python ../util/plot_tcpprobe.py -f $odir/tcp_probe.txt -o $odir/cwnd.png
+	bw=$1
+	odir=dctcp-n$n-bw$bw
+	sudo python dctcp.py --bw $bw --maxq $maxq --dir $odir -t $t -n $n --dctcp
+	sudo python ../util/plot_rate.py --maxy $bw -f $odir/txrate.txt -o $odir/rate.png
+	sudo python ../util/plot_queue.py --maxy 50 -f $odir/qlen_s1-eth1.txt -o $odir/qlen.png
+	sudo python ../util/plot_tcpprobe.py -f $odir/tcp_probe.txt -o $odir/cwnd.png
 }
 
-tcp
-#ecn
-dctcp
+for bw in $bws; do
+    tcp $bw
+    #ecn $bw
+    #dctcp $bw
+done
