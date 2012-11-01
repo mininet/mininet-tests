f8cbb9003e3a967f64013910e11ac5eeae10f748
Vimalkumar <j.vimal@gmail.com>
2012-10-31 23:54:50 -0700

    Generate new DCTCP graphs by reducing number of data points plotted.

--- dctcp/results/gen.sh
+++ dctcp/results/gen.sh
@@ -3,7 +3,7 @@
 pq="python ../../util/plot_queue.py"
 
 for opts in "--cdf" ""; do
-	$pq  -f qlen-mn-dctcp.txt q-dctcp-plot.txt -l Mininet-HiFi Hardware $opts -o dctcp$opts.pdf --maxy 40
+	$pq  -f qlen-mn-dctcp.txt q-dctcp-plot.txt -l Mininet-HiFi Hardware $opts -o dctcp$opts.pdf --maxy 40 --every 5
 	$pq  -f qlen-mn-tcp.txt q-tcp-plot.txt -l Mininet-HiFi Hardware $opts -o tcp$opts.pdf --maxy 600
 done
 
